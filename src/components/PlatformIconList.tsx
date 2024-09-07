75727e82061f23e5385dcff43fdf930e210ee181
Asagar Ali <asagarali@Asagars-MacBook-Air.local>
2024-09-07 19:58:43 +1000

    platform icon list

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,40 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import React from "react";
+import { Platform } from "../hooks/useGames";
+import { IconType } from "react-icons/lib";
+import { HStack, Icon } from "@chakra-ui/react";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    ios: MdPhoneIphone,
+    android: FaAndroid,
+    nintendo: SiNintendo,
+    linux: FaLinux,
+    web: BsGlobe,
+  };
+  return (
+    <HStack marginY={2}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
