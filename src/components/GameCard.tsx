ed3b55cbcdebd203fd24ad00e165ed6c2054fc34
Asagar Ali <asagarali@Asagars-MacBook-Air.local>
2024-09-14 17:38:17 +1000

    filter game by genre

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,5 @@
-
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, HStack, Heading, Image,} from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import ScoreCritics from "./ScoreCritics";
 import getCroppedImage from "../services/image_url";
@@ -11,21 +10,18 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <>
-    <Card >
-      <Image src={getCroppedImage(game.background_image)} />
-      <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent='space-between'>
-        <PlatformIconList
-          platforms={game.parent_platforms.map(p => p.platform)}
-        />
-        <ScoreCritics score = {game.metacritic} />
-        </HStack>
-      </CardBody>
-    </Card>
-    </>
-    
+      <Card>
+        <Image src={getCroppedImage(game.background_image)} />
+        <CardBody>
+          <Heading fontSize="2xl">{game.name}</Heading>
+          <HStack justifyContent="space-between">
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <ScoreCritics score={game.metacritic} />
+          </HStack>
+        </CardBody>
+      </Card>
   );
 };
 
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 
 interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-const useData = <T>(endpoint: string) => {
+const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
   const [data, setData] = useState<T[]>([]);
-  const [error, setError] = useState(" ");
+  const [error, setError] = useState("");
   const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -17,7 +17,7 @@ const useData = <T>(endpoint: string) => {
 
     setLoading(true);
     apiClient
-      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
+      .get<FetchResponse<T>>(endpoint, { signal: controller.signal, ...requestConfig })
       .then((res) => {
         setData(res.data.results);
         setLoading(false);
@@ -29,11 +29,7 @@ const useData = <T>(endpoint: string) => {
       });
 
     return () => controller.abort();
-  }, []);
-  return {
-    data,
-    error,
-    isLoading,
-  };
+  }, deps ? [...deps] : []);
+  return { data, error, isLoading };
 };
 export default useData;
