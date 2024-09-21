2a312ffc1ff92f1b155fae57c738845e0f2d16a5
Asagar Ali <asagarali@Asagars-MacBook-Air.local>
2024-09-21 18:51:19 +1000

    add emojis

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import ScoreCritics from "./ScoreCritics";
 import getCroppedImage from "../services/image_url";
+import Emoji from "./Emoji";
 
 interface Props {
   game: Game;
@@ -10,18 +11,21 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-      <Card>
-        <Image src={getCroppedImage(game.background_image)} />
-        <CardBody>
-          <HStack justifyContent="space-between" marginBottom={4}>
-            <PlatformIconList
-              platforms={game.parent_platforms.map((p) => p.platform)}
-            />
-            <ScoreCritics score={game.metacritic} />
-          </HStack>
-          <Heading fontSize="2xl">{game.name}</Heading>
-        </CardBody>
-      </Card>
+    <Card>
+      <Image src={getCroppedImage(game.background_image)} />
+      <CardBody>
+        <HStack justifyContent="space-between" marginBottom={4}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <ScoreCritics score={game.metacritic} />
+        </HStack>
+        <Heading fontSize="2xl">
+          {game.name}
+          <Emoji rating={game.rating_top} />
+        </Heading>
+      </CardBody>
+    </Card>
   );
 };
 
